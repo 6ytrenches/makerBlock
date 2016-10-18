5662eed62db7181064c8e35c458af4a67a8309da
6ytrenches <miguemyers8@gmail.com>
2016-10-18 10:28:34 -0400

    personal page update'

--- app.rb
+++ app.rb
@@ -5,57 +5,45 @@ require './models'
 set :database, "sqlite3:uncubed.sqlite3"
 
 $menu = [
+  {page: 'home', href: '/home'},
   {page: 'main', href: '/generalPage'},
-  {page: 'home', href: '/home'}
+  {page: 'profile', href: '/personal'}
   ]
   
- get '/' do 
-  @body_class ='home'
+  get '/' do 
+  @users = User.all
   erb :home
 end
 
-# ------------------------
 
-
-get '/personal/:id' do 
-  @users = User.find(params[:id])
-  erb :personal
-end
-
-# get '/company/:id' do 
-#   @company = Company.find(params[:id])
-#   erb :company_profile
-# end
-
-
-get '/' do 
-@regus = User.find_by(email: @user)
+ get '/home' do 
+  @regus = User.find_by(email: @user)
  erb :home
 end
 
-post '/' do
-  
-	# a = params["bob"].to_s
+
+
+post '/home' do
+  # a = params["bob"].to_s
   @regus = User.create(params)
   @user = @regus.email
   erb :home
 end
-#-------------------------
-
-
 
 
 # ------------------------
 
 
-get '/personal' do 
-
- erb :personal
+get '/user/:id' do 
+  @user = User.find(params[:id])
+  erb :personal
 end
 
-post '/personal' do
+post '/user' do
  erb :personal 
 end
+
+
 #-------------------------
 
 get '/posts' do
