4eff7dbc0e811c575a28b01dcb561be6e6e5abed
Yerlan <bayekeshov@gmail.com>
2016-10-21 15:39:06 -0400

    Hide logform

--- app.rb
+++ app.rb
@@ -182,3 +182,11 @@ else
 end
 end
 
+helpers do
+  def current_user 
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+end
+
--- public/main.js
+++ public/main.js
@@ -25,7 +25,6 @@ var imageRotator = {
 imageRotator.initialze();
 
 
-
 });
 
 
